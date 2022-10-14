27587d57d934d17b6aca56e5bd449cf3eaf6616c
Jesper Dybdahl <jdy@obtain.dk>
2022-10-14 13:10:46 +0200

    Add project files.

---
+++ GameOfLifeConsole/GameOfLife.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace GameOfLifeConsole;
+
+public class GameOfLifeDrawEventArgs : EventArgs
+{
+	public int X { get; set; }
+	public int Y { get; set; }
+	public bool Alive { get; set; }
+}
+
+public class GameOfLife
+{
+	public int Wait { get; set; } = 1000;
+	private int width;
+	private int height;
+
+    public bool[,] Grid { get; set; }
+
+	public event EventHandler<GameOfLifeDrawEventArgs> DrawCell;
+
+	public GameOfLife(int width = 40, int height = 20)
+	{
+		this.width = width;
+		this.height = height;
+		Grid = new bool[width, height];
+	}
+
+	public void BuildRandom()
+	{
+		Random rand = new Random((int)DateTime.Now.Ticks);
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				Grid[x,y] = rand.Next(0,2) == 1;
+			}
+		}
+	}
+
+	protected int CountNeighbours(bool[,] grid, int x, int y)
+	{
+		int neighbours = 0;
+
+		for (int ny = -1; ny <= 1; ny++)
+		{
+			for (int nx = -1; nx <= 1; nx++)
+			{
+				if (nx == 0 && ny == 0)
+				{
+					continue;
+				}
+
+				int nPosX = x + nx;
+				int nPosY = y + ny;
+
+				if (nPosX >= 0 && nPosX < this.width && nPosY >= 0 && nPosY < this.height)
+				{ 
+					if (grid[nPosX, nPosY] == true)
+					{
+						neighbours++;
+					}
+				}
+			}
+		}
+
+		return neighbours;
+	}
+
+	protected void NextGeneration()
+	{
+		bool[,] nextGen = new bool[width, height];
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+                nextGen[x, y] = false;
+                
+				int n = this.CountNeighbours(Grid, x, y);
+				
+				if (Grid[x,y] == true)
+				{
+					if (n == 2 || n == 3)
+					{
+						nextGen[x,y] = true;
+					}
+				}
+				else
+				{
+					if (n == 3)
+					{
+						nextGen[x,y] = true;
+					}
+				}
+			}
+		}
+
+		Grid = nextGen;
+	}
+
+	protected void Draw()
+	{
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+                GameOfLifeDrawEventArgs args = new GameOfLifeDrawEventArgs { X = x, Y = y };
+				args.Alive = Grid[x,y];
+
+				DrawCell?.Invoke(this, args);
+            }
+		}
+	}
+
+	public void Run(int generations = 10)
+	{
+		this.BuildRandom();
+		this.Draw();
+
+		for (int gen = 1; gen <= generations; gen++)
+		{
+			System.Threading.Thread.Sleep(Wait);
+			this.NextGeneration();
+			this.Draw();
+		}
+	}
+
+}
---
+++ GameOfLifeConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.SetWindowSize(160,50);
+Console.Clear();
+
+GameOfLifeConsole.GameOfLife gol = new GameOfLifeConsole.GameOfLife(40,40);
+gol.DrawCell += Gol_DrawCell;
+gol.Wait = 500;
+gol.Run(100);
+
+void Gol_DrawCell(object? sender, GameOfLifeConsole.GameOfLifeDrawEventArgs e)
+{
+    Console.SetCursorPosition(e.X*2, e.Y);
+    Console.Write(e.Alive ? "██" : "░░");
+}
